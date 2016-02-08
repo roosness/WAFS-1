3a2c68a829de9d849c58c48fc9159176287239fe
Raymond <raymondkorrel@hotmail.com>
2016-02-08 21:15:58 +0100

    templating & routing

--- opdracht5/static/js/script.js
+++ opdracht5/static/js/script.js
@@ -5,76 +5,73 @@
 	// The application object
 	var app = {
 		init: function() {
-			routes.init();
+			el.init();
+			// Set hash to searh
+			navigation.init();
 			search.init();
 		}
 	};
 
-	// Object for routes
-	var routes = {
+	// Object where elements are declared
+	var el = {
 		init: function() {
-			// Event listener for when the hash changes
-			window.addEventListener('hashchange', navigation.toggle.bind(navigation), false);
-			// Event listener at refresh
- 			window.addEventListener('load', navigation.toggle.bind(navigation), false);
-		}
-	};
-
-	// Objects for showing and hiding navigation as navigation
-	var navigation = {
-		toggle: function() {
-			var hash = window.location.hash;
-			// If there's a hash in the url, hide all screens and show the screen with the current hash
-			if (hash) {
-				var show = document.querySelector(hash);
-				// Check if page exists. If not, alert a message and set hash to start screen
-				if (show) {
-					// Loop through all navigation and set them to display none
-					var screens = document.querySelectorAll('body > section');
-					for (var i = 0; i < screens.length; i++) {
-						screens[i].style.display = 'none';
-					};
-					// Show the element wich has the same id as the hash
-					show.style.display = '';
-				} else {
-					alert('This page does not exist');
-					window.location.hash = '#search';
-				};		
-			} else {
-				// If there's no hash, set hash to start-screen
-				window.location.hash = '#search';
-			};
-			// Execute navigation.navigation
-			this.activeButton();
-		},
-		activeButton: function() {
-			var hash = window.location.hash;
-			var ahref = document.querySelectorAll('.menu-button a');
-			// Search for a elements in .menu-button and give the parent of the a with the current hash value in href the class active-menu-button. Else remove active-menu-button
-			for (var i = 0; i < ahref.length; i++) {
-				var hrefValue = ahref[i].getAttribute('href');
-				if (hrefValue == hash) {
-					ahref[i].parentElement.classList.add("active-menu-button");
-				} else {
-					ahref[i].parentElement.classList.remove("active-menu-button");
-				};
-			};
-		}
-	};
-
-	// Object for searching movies
-	var search = {
-		searchResults: [],
-		init: function() {
-			this.elements();
-			this.submit();
-		},
-		elements: function() {
-			// Declare elements
+			this.searchScreen = document.getElementById('search');
+			this.watchlistScreen = document.getElementById('watchlist');
+			this.navSearch = document.getElementById('nav-search');
+			this.navWatchlist = document.getElementById('nav-watchlist');
 			this.list = document.getElementById('list');
 			this.form = document.getElementById('form');
 			this.submitButton = document.getElementById('submit');
 			this.searchvalue = document.getElementById('searchvalue');
+			this.firstLih1 = document.querySelector('#list li:first-child h1');
+			this.loading = document.querySelector('.loading');
+		}
+	}
+
+	// Object where Routie detects the hashes and displays the right section
+	var navigation = {
+		init: function() {
+			// If no hash, set the hash to #search
+			if (!window.location.hash) {
+				routie('search');
+			};
+
+			// Let Routie detect the hash value.
+			routie({
+			    search: function() {
+			    	// When the hash is search, show the search section and make the right button active
+					el.searchScreen.style.display = '';
+					el.watchlistScreen.style.display = 'none';
+					el.navSearch.classList.add('active-menu-button')
+					el.navWatchlist.classList.remove('active-menu-button')
+			    },
+			    watchlist: function() {
+			    	// When the hash is watchlist, show the watchlist section and make the right button active
+					el.searchScreen.style.display = 'none';
+					el.watchlistScreen.style.display = '';
+					el.navSearch.classList.remove('active-menu-button')
+					el.navWatchlist.classList.add('active-menu-button')
+			    },
+			})
+		}
+	}
+	
+	// Object for searching movies
+	var search = {
+		searchResults: [],
+		init: function() {
+			this.submit();
+			this.emptyList();
+			// Hide the loading gif by default
+			el.loading.style.display = 'none'
+		},
+		emptyList: function() {
+			// If the list is empty, hide the list. If not, show the list
+			if (el.firstLih1 && el.firstLih1.value) {
+				el.list.style.display = '';
+			} else {
+				el.list.style.display = "none";
+			}
 		},
 		apiCall: function(search) {
 			// Declare new Promise function
@@ -96,10 +93,10 @@
 				    	if( (status >= 200 && status < 300) || status === 304 ) {
 				    		var json = JSON.parse(xhr.responseText);
 				    		// Tell the promise it succeded and return the object
-				        	resolve(json.Search);
+				        	resolve(json);
 				    	} else {
 				    		// Tell the promise an error occurred
-				    		reject();
+				    		reject(json);
 				    	}
 				    }
 				}
@@ -109,9 +106,12 @@
 		pushToArray: function(search) {
 			// Fire apiCall with the search string
 			this.apiCall(search).then(function (object) {
-				// When the data is succesfully received, object = searchResults. Then render
+				// When the data is succesfully received, object = searchResults. Then render and display the list
 				this.searchResults = object;
 				this.render();
+				el.list.style.display = '';
+				el.loading.style.display = 'none';
+
 			}.bind(this))
 			.catch(function() {
 				// If an error occurred, alert there is something wrong
@@ -120,62 +120,52 @@
 		},
 		submit: function() {
 			// Add event listener to submit of the form button
-			this.form.addEventListener("submit", this.getValue.bind(this) )
+			el.form.addEventListener("submit", this.getValue.bind(this) )
 		},
 		loading: function(elem) {
-			// Delete everything in the list
-			while (elem.firstChild) {
-			    elem.removeChild(elem.firstChild);
-			}
+			// Hide the list
+			el.list.style.display = 'none';
 
-			// Append a loading gif while waiting
-			var img = document.createElement("img");
-			img.setAttribute('src', 'static/img/loading.gif');
-
-			elem.appendChild(img);
+			// Show the loading gif while waiting
+			el.loading.style.display = '';
 		},
 		getValue: function(e) {
 			// Prevent refreshing
 			e.preventDefault();
 			// Get value of the search input
-			var value = this.searchvalue.value;
+			var value = el.searchvalue.value;
 			// Fire loading and pushToArray
-			this.loading(this.list);
+			this.loading(el.searchScreen);
 			this.pushToArray(value);
 		},
 		render: function() {
-			// Delete all elements in the list
-			while (this.list.firstChild) {
-			    this.list.removeChild(this.list.firstChild);
-			}
+			// Filter out all the movies without an image
+			var goodImages = this.searchResults.Search.filter(function(out) { 
+				return out.Poster != 'N/A'; 
+			})
+			// Place the object as the new searchresults object
+			this.searchResults = goodImages;
 
-			// Create elements for every search result and append to the list
-			for (var i = 0; i < this.searchResults.length; i++) {
-				var li = document.createElement("li");
-				li.setAttribute('id', 'searchLi-'+this.searchResults[i].imdbID)
-				li.setAttribute('value', this.searchResults[i].imdbID)
-				var div = document.createElement("div");
-				var h1 = document.createElement("h1");
-				var img = document.createElement("img");
-				img.setAttribute('src', this.searchResults[i].Poster);
-				var span = document.createElement("span");
-				var checkbox = document.createElement("input");
-				checkbox.setAttribute('type', 'checkbox');
-				checkbox.setAttribute('id', 'checkboxSearch-'+this.searchResults[i].imdbID);
-				checkbox.setAttribute('value', this.searchResults[i].imdbID);
-				var label = document.createElement('label');
-				label.setAttribute('for', 'checkboxSearch-'+this.searchResults[i].imdbID);
-				var year = document.createTextNode(this.searchResults[i].Year);
-				var title = document.createTextNode(this.searchResults[i].Title);
+			// Declare all functions to get data from object
+			var liId = function() {return 'searchLi-'+this.imdbID}
+			var valueId = function() {return this.imdbID}
+			var title = function() {return this.Title}
+			var img = function() {return this.Poster}
+			var year = function() {return this.Year}
+			var checkboxId = function() {return 'checboxSearch'+this.imdbID}
 
-				li.appendChild(img);
-				li.appendChild(div).appendChild(h1).appendChild(title);
-				div.appendChild(span).appendChild(year);
-				li.appendChild(checkbox);
-				li.appendChild(label);
-				
-				this.list.appendChild(li);
-			}
+			// Object to let Transparency know what values to give which element
+			var directives = {
+			  searchresult: {id: liId, value: valueId},
+			  img: {src: img},
+			  title: {text: title},
+			  year: {text: year},
+			  checkbox: {id: checkboxId},
+			  label: {for: checkboxId}
+			};
+
+			// Render
+			Transparency.render(el.list, this.searchResults, directives);
 		}
 	}
 
